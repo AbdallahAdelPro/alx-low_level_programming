54d6d5efae5b09e399a754c287c2a99f0b1d2e94
Abdallah <abdallah.adel1221@gmail.com>
2023-03-17 11:35:35 -0700

    revers alpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char *alpha_revers = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 26; i => 0; i--)
+		putchar(alpha_revers[i]);
+	putchsr('\n');
+	return (0);
+}
