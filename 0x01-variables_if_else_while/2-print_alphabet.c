0834dcb0f617c3526c1be9de79a0b3bc7d067feb
Abdallah <abdallah.adel1221@gmail.com>
2023-03-17 10:05:40 -0700

    print alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char alpha[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+	int i;
+	for (i = 0; i < 26; i++)
+		putchar(alpha[i]);
+	putchar('\n');
+	return (0);
+}
