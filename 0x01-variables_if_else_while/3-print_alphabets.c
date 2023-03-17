40559315ee55718884a8fb7dd8890027bf495e6a
Abdallah <abdallah.adel1221@gmail.com>
2023-03-17 10:18:08 -0700

    print alpha l&u

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char alpha_L[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g'
+		, 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'
+		, 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\n'
+		,'A', 'B', 'C', 'D', 'E', 'F', 'G'
+		, 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'
+		, 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+	int i;
+
+	for (i = 0; i < 26; i++)
+		putchar(alpha[i]);
+	putchar('\n');
+	return (0);
+}
