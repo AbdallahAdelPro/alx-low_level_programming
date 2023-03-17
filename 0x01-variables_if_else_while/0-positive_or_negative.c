15785b5c29395fa9bd2aba607a31199c113b6d50
Abdallah <abdallah.adel1221@gmail.com>
2023-03-17 09:03:21 -0700

    the sign of the num

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,26 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main function
+ * the code of random num
+ * if the num < 0 
+ * print nigative
+ * if num == 0
+ * print 0
+ * if num > 0
+ * print positive
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	if (n<0)
+		printf(" %d is negative\n", n);
+	else if (n==0)
+		printf("%d is zero\n", n);
+	else 
+		printf("%d is positve\n", n);
 	return (0);
 }
