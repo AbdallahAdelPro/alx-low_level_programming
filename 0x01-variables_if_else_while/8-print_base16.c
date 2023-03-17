9df1801cdce1e314a2435cd38e9f18ca43507686
Abdallah <abdallah.adel1221@gmail.com>
2023-03-17 11:47:11 -0700

    hexa num

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char *hexa = "abcdef";
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(48 + i);
+	for (i = 0; i < 10; i++)
+		putchar(hexa[i]);
+	putchar('\n');
+	return (0);
+}
