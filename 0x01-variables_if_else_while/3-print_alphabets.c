f56c87301514543d0f60a2b7ceb8e196f9045e1d
Abdallah <abdallah.adel1221@gmail.com>
2023-03-17 10:22:43 -0700

    print alpha l&u

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,15 +7,15 @@
  */
 int main(void)
 {
-	char alpha[53] = {'a', 'b', 'c', 'd', 'e', 'f', 'g'
+	char alpha[52] = {'a', 'b', 'c', 'd', 'e', 'f', 'g'
 		, 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'
-		, 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\n'
+		, 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 		,'A', 'B', 'C', 'D', 'E', 'F', 'G'
 		, 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'
 		, 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 	int i;
 
-	for (i = 0; i < 53; i++)
+	for (i = 0; i < 52; i++)
 		putchar(alpha[i]);
 	putchar('\n');
 	return (0);
