93638517bdd6e1470736dbfb82dc846798378eca
Abdallah <abdallah.adel1221@gmail.com>
2023-03-22 16:54:04 -0700

    main.h

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+carrie@ubuntu:/debugging$ cat main.h
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int convert_day(int month, int day)
+
+#endif /* MAIN_H */
