fa9a7eb0ff2449c3d65ac596b5381995d3f50c90
Abdallah <abdallah.adel1221@gmail.com>
2023-03-27 17:45:22 -0700

    the lenth of the string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include"main.h"
+
+/**
+ * _strlen - calculates the string length
+ *
+ * @s: A pointer to the string
+ *
+ * Return: integer (the length of the string)
+ */
+int _strlen(char *s)
+{
+	strlen(s);
+}
