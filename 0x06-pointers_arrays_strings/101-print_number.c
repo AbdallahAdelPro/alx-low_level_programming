1d062a7c27cb8bc142ff5364d7ac7660b2cda580
Abdallah <abdallah.adel1221@gmail.com>
2023-03-30 10:25:58 -0700

    print int

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,15 +8,20 @@
   */
 void print_number(int n)
 {
+	unsigned int x;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		n *= -1;
 	}
-	if (n / 10)
-	{
-		print_number(n / 10);
-	}
-	_putchar((n % 10) + '0');
+
+	x = n;
+
+	if (x / 10)
+		print_number(x / 10);
+
+	_putchar(x % 10 + '0');
+
 
 }
