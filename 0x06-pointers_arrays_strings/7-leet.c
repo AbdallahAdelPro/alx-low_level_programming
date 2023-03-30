b64b946847571b835d6f752a36f6626f0ceb8863
Abdallah <abdallah.adel1221@gmail.com>
2023-03-30 09:02:46 -0700

    encode the code

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,8 +11,8 @@ char *leet(char *s)
 {
 	int i;
 	int j;
-	char a[] = "aAeEoOtTlL"
-	char b[] = "4433007711"
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
