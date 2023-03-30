65cb0c84ee44332f729f8e119624f45f035a4348
Abdallah <abdallah.adel1221@gmail.com>
2023-03-30 09:10:50 -0700

    encode the code

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string
+ *
+ * @s: string to encode
+ *
+ * Return: address of s
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; j <= 52; j++)
+		{
+			if (a[j] == s[i])
+				s[i] = b[j];
+		}
+	}
+	return (s);
+}
