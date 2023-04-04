1d7a3270e53081796687db8eddd8a3843b8aab34
Abdallah <abdallah.adel1221@gmail.com>
2023-04-03 17:28:53 -0700

    locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,19 +6,19 @@
  * @c: the occurrence of the character
  * @s: the string
  *
- * Return: NULL
+ * Return: *s
  */
 char *_strchr(char *s, char c)
 {
-	unsigned int i;
+        unsigned int i;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] == c)
-		{
-			return (&s[i]);
-		}
-	}
-	return (NULL);
+        for (i = 0; s[i] != '\0'; i++)
+        {
+                if (s[i] == c)
+                {
+                        return (&s[i]);
+                }
+        }
+        return (NULL);
 
 }
