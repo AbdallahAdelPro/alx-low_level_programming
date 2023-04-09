34a2c0e0b2d658b5e18371be3ad92891c3a4f0eb
Abdallah <abdallah.adel1221@gmail.com>
2023-04-09 16:11:16 -0700

    multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+/**
+ * main - the main function
+ *
+ * @argc: the length of the argument command
+ * @argv: an array of strings
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int mul = 1;
+
+	if (argc == 2)
+	{
+		for (i = 0; i < argc; i++)
+		{
+		mul *= atoi(argv[i]);
+		}
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
