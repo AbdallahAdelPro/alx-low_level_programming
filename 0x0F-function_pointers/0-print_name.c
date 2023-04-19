0e6a3dce4949ca504f938c6e75992c93737a1635
Abdallah <abdallah.adel1221@gmail.com>
2023-04-19 14:32:17 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+ * main - the main function
+ *
+ * @name: the pointer that point to the name
+ * @f: pointer that point to the function
+ *
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
