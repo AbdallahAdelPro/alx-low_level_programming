5657d7afde3fdd405f7644c1921f897d23ca7fca
Abdallah <abdallah.adel1221@gmail.com>
2023-04-29 07:50:08 -0700

    print list of int

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,17 +10,18 @@
 size_t print_listint(const listint_t *h)
 {
 
-	size_t count = 0;
+	size_t count = 1;
 
 	if (h == NULL)
 		return (0);
 
-	while (h != NULL)
+	while (h->next != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		count++;
 	}
+	printf("%d\n", h->n);
 
 	return (count);
 
