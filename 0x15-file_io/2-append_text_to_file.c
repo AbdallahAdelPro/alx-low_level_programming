a9e29857088a3dd0f77d43821f254d92d99849ff
Abdallah <abdallah.adel1221@gmail.com>
2023-05-08 17:04:30 -0700

    up date it

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -3,7 +3,7 @@
 /**
  * append_text_to_file - appends text at the end of a file
  * @filename: name of the file
- * @text_content the NULL terminated string to add at the end of the file
+ * @text_content: the NULL terminated string to add at the end of the file
  * Return: 1 on success and -1 on failure
  */
 
